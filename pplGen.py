ba596aa4d53dbc373586a6189540306f4e92eca1
aash29 <aash29@gmail.com>
2016-04-23 14:25:37 +0300

    sqlite interface

--- pplGen.py
+++ pplGen.py
@@ -2,6 +2,13 @@
 import os
 import random
 import sqlite3
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+
+c.execute('DROP TABLE IF EXISTS ppl')
+
+c.execute('''CREATE TABLE ppl
+             (id integer, name text, surname text)''')
 
 with open(os.getcwd()+os.sep+'male_names.txt',encoding="utf8") as maleNamesFile:
     maleNames = maleNamesFile.read().splitlines()
@@ -15,5 +22,22 @@ globalMap=[]
 
 for i in range(1,10):
     globalMap.append(dict({'name':random.choice(maleNames),'surname':random.choice(maleSurnames)}))
+    c.execute('insert into ppl values (?,?,?)', [i, globalMap[-1]['name'], globalMap[-1]['surname']])
 
-print(globalMap);
+
+
+c.execute('SELECT * FROM ppl')
+# 1 способ напечатать результат
+meida = c.fetchall()
+print(meida)
+# 2 способ напечатать результат
+for row in c.execute("select name, surname from ppl"):
+    print(row)
+
+conn.commit();
+
+
+#print(globalMap);
+
+
+conn.close();
